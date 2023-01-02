f122f058cd02daad1dbb3ffa32056f6adf2138fa
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2023-01-02 09:47:58 -0300

    Update testePokemon.py

--- Programador de Sistemas - UC 1/Repositório de Exemplos/Revisão Pós Recesso/testePokemon.py
+++ Programador de Sistemas - UC 1/Repositório de Exemplos/Revisão Pós Recesso/testePokemon.py
@@ -1,3 +1,10 @@
+# Crie um Pokemon baseado em uma lista de nomes, de forma aleatória
+
+# Gere o ataque, o hp e a defesa desse pokemon de forma aleatória, dependendo
+# da sua espécie.
+
+# Crie 2 pokemons e compare o mais forte.
+
 import random
 
 especiesPokemon = ("Pikachu","akopskaposkpaos")
