7f8820c5132091e38844ff6a614962ceeeda7ff9
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2023-02-01 11:34:02 -0300

    Add files via upload

--- Programador de Sistemas - UC2/Repositório de Exemplos/Aula 01-02/main.py
+++ Programador de Sistemas - UC2/Repositório de Exemplos/Aula 01-02/main.py
@@ -6,14 +6,42 @@ import psycopg2
 try:
     con = Conexao("Empresa", "localhost", "5432", "postgres", "postgres")
 
-    funcionario = Funcionario(2, "Getulio","05955150374","5000",idDepartamento="3")
-    funcionario.imprimirFuncionario()
-    con.manipularBanco(funcionario.atualizarFuncionario("Funcionarios"))
 
-    print(con.consultarBanco('''
+    listaFuncionarios = con.consultarBanco('''
     SELECT * FROM "Funcionarios"
-    
-    '''))
+    ORDER BY "ID" ASC
+    ''')
+    print("ID | Nome")
+    for func in listaFuncionarios:
+        print(f"{func[0]} - {func[1]} \n")
+
+    funcionarioEscolhido = input("Escolha o id do funcionario que deseja alterar: ")
+
+    funcionarioConsulta = con.consultarBanco(f'''
+    SELECT * FROM "Funcionarios"
+    WHERE "ID" = '{funcionarioEscolhido}'
+    ''')
+    funcionario = Funcionario(funcionarioConsulta[0][0], funcionarioConsulta[0][1], funcionarioConsulta[0][2], funcionarioConsulta[0][3], funcionarioConsulta[0][4])
+
+    print("Funcionario escolhido foi:")
+    print(funcionario.imprimirFuncionario())
+
+    opcoes = input("Você deseja alterar as informações basicas(1) ou o departamento(2)?")
+
+    match opcoes:
+        case "1":
+            funcionario.nome = input("Escreva o novo nome: ")
+            funcionario.cpf = input("Escreva o novo cpf: ")
+            funcionario.salario = input("Escreva o novo salário: ")
+
+            con.manipularBanco(funcionario.atualizarFuncionario("Funcionarios"))
+        case "2": 
+            funcionario.idDepartamento = input("Insira o id do novo Departamento")
+            con.manipularBanco(funcionario.atualizarDepartamentoFuncionario("Funcionarios"))
+
+    print(funcionario.imprimirFuncionario())
+    print(con.consultarBanco('''Select * from "Funcionarios"'''))
+
 
 
     con._db.close()
