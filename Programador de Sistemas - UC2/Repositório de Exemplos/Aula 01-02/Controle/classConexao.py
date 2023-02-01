55291877bbeee714f93785b09e9e95457c6426c9
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2023-02-01 09:16:34 -0300

    Add files via upload

--- Programador de Sistemas - UC2/Repositório de Exemplos/Aula 01-02/Controle/classConexao.py
+++ Programador de Sistemas - UC2/Repositório de Exemplos/Aula 01-02/Controle/classConexao.py
@@ -5,3 +5,19 @@ class Conexao:
     def __init__(self, parametroDb, parametroHost, parametroPort, parametroUser, parametroPassword):
         self._db = psycopg2.connect(database = parametroDb, host = parametroHost, port = parametroPort, user = parametroUser, password = parametroPassword)
 
+    def consultarBanco(self,sql):
+        cursor = self._db.cursor()
+
+        cursor.execute(sql)
+
+        resultado = cursor.fetchall()
+
+        cursor.close()
+
+        return resultado
+
+    def manipularBanco(self):
+        #Criar cursor
+        #Executar o sql que vem do parâmetro
+        #Fazer o commit da conexão
+        #Fechar o cursor
--- Programador de Sistemas - UC2/Repositório de Exemplos/Aula 01-02/main.py
+++ Programador de Sistemas - UC2/Repositório de Exemplos/Aula 01-02/main.py
@@ -4,12 +4,7 @@ import psycopg2
 
 try:
     con = Conexao("Empresa", "localhost", "5432", "postgres", "postgres")
-    cursor = con._db.cursor()
 
-    cursor.execute('''SELECT * FROM "Funcionarios" ''')
-    print(cursor.fetchall())
-    cursor.close()
-    con._db.close()
     
 
 except (Exception, psycopg2.Error) as error:
