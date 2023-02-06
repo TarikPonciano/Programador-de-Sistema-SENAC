e4807c26bde6a715dd51b5291c1d319ccdedd0aa
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2023-02-06 08:18:45 -0300

    Add files via upload

---
+++ Programador de Sistemas - UC2/Repositório de Exemplos/Aula 06-02/Biblioteca/main.py
@@ -0,0 +1,9 @@
+#Aula 06-02
+#Conteúdo: Revisão Geral de Implementação de Banco de Dados com Python
+#Programa: Sistema de Gerenciamento de Biblioteca
+#Objetivos:
+#   - Criar modelo físico de dados
+#   - Entidades: Livros e Clientes Relacionamentos: Aluguel
+#   - Criar banco de dados e tabelas
+#   - Integrar banco com Python e criar funções de manipulação das tabelas
+
