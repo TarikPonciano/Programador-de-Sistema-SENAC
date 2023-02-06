17e8a3e8783bb5d7a5c821b1e4e75d1498e4646a
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2023-02-06 09:13:31 -0300

    Add files via upload

---
+++ Programador de Sistemas - UC2/Repositório de Exemplos/Aula 06-02/Biblioteca/Controle/classConexao.py
@@ -0,0 +1,31 @@
+import psycopg2
+
+class Conexao:
+    def __init__(self, parametroDB, parametroHost, parametroPort, parametroUser,parametroPassword):
+        self._db = psycopg2.connect(database=parametroDB, host=parametroHost, port=parametroPort, user=parametroUser, parametroPassword=parametroPassword)
+
+    def consultarBanco(self, sql):
+        cursor = self._db.cursor()
+
+        resultado = cursor.execute(sql)
+
+        cursor.close()
+
+        return resultado
+
+    def manipularBanco(self, sql):
+        cursor = self._db.cursor()
+
+        cursor.execute(sql)
+
+        self._db.commit()
+
+        cursor.close()
+
+        return True
+
+    def fechar(self):
+
+        self._db.close()
+
+
