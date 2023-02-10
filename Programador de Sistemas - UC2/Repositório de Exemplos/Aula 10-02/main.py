0baf978e1f457e18ee640e401bd9326571188974
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2023-02-10 10:01:02 -0300

    Add files via upload

--- Programador de Sistemas - UC2/Repositório de Exemplos/Aula 10-02/main.py
+++ Programador de Sistemas - UC2/Repositório de Exemplos/Aula 10-02/main.py
@@ -1,24 +1,46 @@
-import PySimpleGUI as sg #pip install pysimplegui
+import PySimpleGUI as sg 
 
 sg.theme("DarkAmber")
 
-layout = [[sg.Push(),sg.MLine(key="-ChatBox-", size=(50,10)),sg.Push()],
-[sg.Text("Chat: "), sg.Input(key="-InputChat-")],
-[sg.Push(),sg.Button("Enviar"),sg.Button("Fechar"),sg.Push()]
+telaInicial = [[sg.Push(),sg.Text("Bem vindo a empresa 'Empresa'"),sg.Push()],
+[sg.Button("Ver Funcionarios",key="-VerFuncionarios-")],
+[sg.Button("Inserir Funcionario",key="-InserirFuncionario-")],
+[sg.Button("Sair", key="-TelaPrincipalSair-")]]
+
+telaInserir = [[sg.Text("Insira um Funcionario")],
+[sg.Text("Nome: "), sg.Input(key="-InserirNomeFuncionario-")],
+[sg.Text("CPF: "), sg.Input(key="-InserirCpfFuncionario-")],
+[sg.Text("Idade: "), sg.Input(key="-InserirIdadeFuncionario-")],
+[sg.Button("Enviar", key="-FuncionarioInserirEnviar-"),sg.Button("Voltar", key="-FuncionarioInserirVoltar-")]
 ]
-chatLog = ''''''
-usuario = "Tarik: "
-window = sg.Window("Chat Simples", layout)
+
+layout = [[sg.Column(telaInicial, key="-TelaInicial-", visible=True), sg.Column(telaInserir, key="-TelaInserir-",visible=False)]]
+
+window = sg.Window("Sistema de Gestão", layout)
 
 while True:
+
     event, values = window.read()
 
-    if event in ('Fechar', sg.WIN_CLOSED):
+    if event in ("-TelaPrincipalSair-", sg.WIN_CLOSED):
         break
-    if event == "Enviar":
-        texto = values["-InputChat-"]
-        chatLog = chatLog + usuario + texto + "\n"
-        window["-InputChat-"].update('')
-        window["-ChatBox-"].update(chatLog)
+    if event == "-InserirFuncionario-":
+        window['-TelaInicial-'].update(visible=False)
+        window['-TelaInserir-'].update(visible=True)
+
+    if event =="-FuncionarioInserirVoltar-":
+        window['-TelaInicial-'].update(visible=True)
+        window['-TelaInserir-'].update(visible=False)
+    
+    if event =="-FuncionarioInserirEnviar-":
+        nomeFuncionario = values["-InserirNomeFuncionario-"]
+        cpfFuncionario = values["-InserirCpfFuncionario-"]
+        idadeFuncionario = values["-InserirIdadeFuncionario-"]
+
+        print(f'''
+        Nome: {nomeFuncionario}
+        CPF: {cpfFuncionario}
+        Idade: {idadeFuncionario}
+        ''')
 
 window.close()
