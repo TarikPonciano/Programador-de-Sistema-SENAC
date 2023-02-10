221cb8c09ec671469b23c4da6c84823534da8091
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2023-02-10 09:13:28 -0300

    Add files via upload

--- Programador de Sistemas - UC2/Repositório de Exemplos/Aula 10-02/main.py
+++ Programador de Sistemas - UC2/Repositório de Exemplos/Aula 10-02/main.py
@@ -6,8 +6,9 @@ layout = [[sg.Push(),sg.MLine(key="-ChatBox-", size=(50,10)),sg.Push()],
 [sg.Text("Chat: "), sg.Input(key="-InputChat-")],
 [sg.Push(),sg.Button("Enviar"),sg.Button("Fechar"),sg.Push()]
 ]
-
-window = sg.Window("Nova janela", layout)
+chatLog = ''''''
+usuario = "Tarik: "
+window = sg.Window("Chat Simples", layout)
 
 while True:
     event, values = window.read()
@@ -16,7 +17,8 @@ while True:
         break
     if event == "Enviar":
         texto = values["-InputChat-"]
-        print(texto)
+        chatLog = chatLog + usuario + texto + "\n"
         window["-InputChat-"].update('')
+        window["-ChatBox-"].update(chatLog)
 
 window.close()
