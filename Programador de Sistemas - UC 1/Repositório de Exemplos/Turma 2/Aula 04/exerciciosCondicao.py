6b8533fe7249003777da2e430e81d12206f08d90
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2023-02-24 14:14:33 -0300

    Add files via upload

--- Programador de Sistemas - UC 1/Repositório de Exemplos/Turma 2/Aula 04/exerciciosCondicao.py
+++ Programador de Sistemas - UC 1/Repositório de Exemplos/Turma 2/Aula 04/exerciciosCondicao.py
@@ -56,4 +56,72 @@ def questao4Complexa():
     else:
         print("Você escreveu números iguais.")
 
-questao4Complexa()
+#Questão Lanchonete - Escrever um algoritmo que leia o código do item pedido, a quantidade e
+#calcule o valor a ser pago por aquele lanche. Considere que a cada
+#execução somente será calculado um item.
+def questaoLanchonete():
+
+    print('''
+    
+    Especificação Preço unitário
+    100 Cachorro quente 1,10
+    101 Bauru simples 1,30
+    102 Bauru c/ovo 1,50
+    103 Hamburger 1,10
+    104 Cheeseburger 1,30
+    105 Refrigerante 1,00
+    
+    
+    ''')
+
+    codigo = input("Insira o código do produto escolhido: ")
+    
+    quantidade = int(input("Quantos deseja comprar: "))
+
+    preco = 0
+    nome = ""
+
+    if codigo == "100":
+        nome = "Cachorro Quente"
+        preco = 1.10
+    elif codigo == "101":
+        nome = "Bauru Simples"
+        preco = 1.30
+    elif codigo == "102":
+        nome = "Bauru c/ ovo"
+        preco = 1.50
+    elif codigo == "103":
+        nome = "Hamburguer"
+        preco = 1.10
+    elif codigo == "104":
+        nome = "Cheeseburguer"
+        preco = 1.30
+    elif codigo == "105":
+        nome = "Refrigerante"
+        preco = 1.00
+
+    # match codigo:
+    #     case "100":
+    #         nome = "Cachorro Quente"
+    #         preco = 1.10
+    #     case "101":
+    #         nome = "Bauru Simples"
+    #         preco = 1.30
+    #     case "102":
+    #         nome = "Bauru c/ ovo"
+    #         preco = 1.50
+    #     case "103":
+    #         nome = "Hamburguer"
+    #         preco = 1.10
+    #     case "104":
+    #         nome = "Cheeseburguer"
+    #         preco = 1.30
+    #     case "105":
+    #         nome = "Refrigerante"
+    #         preco = 1.00
+        
+
+    valorTotal = preco * quantidade
+    print(f"Você comprou {quantidade} {nome} por R$ {valorTotal}")
+
+questaoLanchonete()
