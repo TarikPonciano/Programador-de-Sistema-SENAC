ef06974fed0c8a4737f163721c4c5dfdfeb0172b
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2023-03-01 10:58:16 -0300

    Add files via upload

--- Programador de Sistemas - UC 3/Repositório de Exemplos/Aula 2/Flask Introdução/main.py
+++ Programador de Sistemas - UC 3/Repositório de Exemplos/Aula 2/Flask Introdução/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import psycopg2
 
 app = Flask(__name__)
 
@@ -8,7 +9,16 @@ def home():
 
 @app.route("/Pokemons")
 def verPokemons():
-    return "Pokemons aqui"
+    try:
+        con = psycopg2.connect(database="Pokemon",user = "postgres",password = "postgres", host="localhost", port="5432")
+        cursor = con.cursor()
+        cursor.execute('''SELECT * FROM "Pokemons"
+        ORDER BY "Id" ASC''')
+        pokemons = cursor.fetchall()
+        
+        return render_template("pokemons.html", pokemons=pokemons)
+    except(Exception, psycopg2.Error) as error:
+        return f"Ocorreu um erro {error}"
 
 if __name__ == "__main__":
     app.run(debug=True)
