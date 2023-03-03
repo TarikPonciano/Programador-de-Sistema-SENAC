84ae05f5489cf629d5a1b45a17f0c2e451e09b75
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2023-03-03 08:33:23 -0300

    Delete Programador de Sistemas - UC 3/Aula 3 directory

--- Programador de Sistemas - UC 3/Aula 3/Teste/Cliente/cliente.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-requisicao = requests.get("http://127.0.0.1:5000/Pokemons")
-
-jsonObj = {'mensagem': "teste"}
-requests.post("http://127.0.0.1:5000/Pokemons/Inserir",json=jsonObj)
-
-
-print(requisicao.json())
