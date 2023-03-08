7add96684a94104950a968c99f35348028a3d671
Tarik Ponciano <tarikponciano@gmail.com>
2023-03-08 10:39:32 -0300

    Create questoes.py

---
+++ Programador de Sistemas - UC 3/Repositório de Exemplos/Aula 5/questoes.py
@@ -0,0 +1,5 @@
+# Crie um programa que receba uma lista de números e retorne uma nova lista com os valores ordenados de forma crescente, sem utilizar a função sort() do Python.
+# Escreva um programa que leia um arquivo de texto e retorne a quantidade de palavras diferentes que aparecem no arquivo.
+# Crie uma função que receba uma lista de números e retorne uma nova lista com os valores únicos da lista original, sem utilizar a função set() do Python.
+# Faça um programa que leia um arquivo de texto e retorne a palavra mais frequente no arquivo.
+# Escreva um programa que receba uma lista de números e retorne a soma dos valores que se encontram em posições í
