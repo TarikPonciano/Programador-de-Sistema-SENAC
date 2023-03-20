aa4bfc64f15191dec34b777e7cd7912f171a8c56
Tarik Ponciano <tarikponciano@gmail.com>
2023-03-20 15:49:10 -0300

    Update ExerciciosPOO 2.py

--- Programador de Sistemas - UC 1/Repositório de Exemplos/Turma 2/Aula 14/ExerciciosPOO 2.py
+++ Programador de Sistemas - UC 1/Repositório de Exemplos/Turma 2/Aula 14/ExerciciosPOO 2.py
@@ -5,13 +5,17 @@ class Agenda:
         self.maxContatos = maxContatos
 
     def adicionarContato(self, novoContato):
-        self.listaDeContatos.append(novoContato)
+        if len(self.listaDeContatos) < self.maxContatos:
+            self.listaDeContatos.append(novoContato)
+        else:
+            print("Contato não foi adicionado. Limite atingido.")
         
 
     def removerContato(self,nomeContato):
         for contato in self.listaDeContatos:
             if contato.nome == nomeContato:
                 self.listaDeContatos.remove(contato)
+
 
     def informacoes(self):
         
@@ -33,4 +37,22 @@ contato1 = Contato("Jorge","1o23-123i9-123")
 contato2 = Contato("Maicou", "1230140819841982")
 
 contato3 = Contato("Maria", "1231151512")
+
+minhaAgenda = Agenda([contato1,contato2,contato3], 5)
+
+print(minhaAgenda.informacoes())
+
+minhaAgenda.removerContato("Maria")
+
+print(minhaAgenda.informacoes())
+
+minhaAgenda.adicionarContato(Contato("Josuel", "1231411"))
+
+minhaAgenda.adicionarContato(Contato("Milena", "1231412e121"))
+
+print(minhaAgenda.informacoes())
+
+minhaAgenda.adicionarContato(Contato("Fabricio","12312d12e1"))
+
+minhaAgenda.adicionarContato(Contato("Pericles", "1djoi1jd12"))
 
