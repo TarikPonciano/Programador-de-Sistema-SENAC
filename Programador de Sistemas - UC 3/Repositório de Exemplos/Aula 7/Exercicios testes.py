0615b6e544c9c175af3a0887a86be87e9afe1d55
Tarik Ponciano <tarikponciano@gmail.com>
2023-03-20 08:23:31 -0300

    Exercícios Testes

---
+++ Programador de Sistemas - UC 3/Repositório de Exemplos/Aula 7/Exercicios testes.py
@@ -0,0 +1,20 @@
+'''
+Por conta própria, pesquise os seguintes assuntos:
+
+- Testes de Software
+- Plano de Testes
+- Casos de Testes
+- Teste Unitários (Mais importante)
+- PyUnit (Mais importante)
+
+Utilize esses conhecimentos para tentar solucionar os seguintes problemas:
+
+Crie uma classe Retangulo que possua os atributos largura e altura e os métodos area() e perimetro(). Em seguida, crie uma classe de testes unitários para verificar se os métodos area() e perimetro() estão retornando os valores corretos.
+
+Crie uma função fatorial(n) que calcule o fatorial de um número n. Em seguida, crie uma classe de testes unitários para verificar se a função está retornando o resultado correto para alguns valores de entrada.
+
+Crie uma função ordena_lista(lista) que receba uma lista de números e retorne uma nova lista ordenada em ordem crescente. Em seguida, crie uma classe de testes unitários para verificar se a função está ordenando corretamente alguns exemplos de listas de entrada.
+
+Crie uma função calcula_media(numeros) que calcule a média aritmética de uma lista de números. Em seguida, crie uma classe de testes unitários para verificar se a função está retornando a média correta para alguns exemplos de listas de entrada.
+
+'''
