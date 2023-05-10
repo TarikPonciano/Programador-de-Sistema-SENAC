4b95fb49f53d63195e526571a6c71c8790047bf0
Tarik Ponciano <tarikponciano@gmail.com>
2023-05-10 14:29:33 -0300

    Update main.py

--- Programador de Sistemas - UC 2/Repositório de Exemplos - Turma 2/Aula 14/main.py
+++ Programador de Sistemas - UC 2/Repositório de Exemplos - Turma 2/Aula 14/main.py
@@ -21,14 +21,24 @@ larguraJanela = 800
 alturaJanela = 600
 app.geometry(f"{larguraJanela}x{alturaJanela}")
 
-app.grid_columnconfigure((0), weight=1)
+app.grid_columnconfigure((0,1), weight=1)
 
+def cliqueInserir():
+    texto = inserirTexto.get()
+    corpoDoTexto.configure(text=texto)
 
 tituloTopo = CTkLabel(master=app, text= "Olá Mundo!", text_color="red", font=CTkFont(size=36,weight="bold"))
 tituloTopo.grid(row=0,column=0, padx=20, pady=20)
 
 corpoDoTexto = CTkLabel(master=app, text="Este é meu programa!", font=CTkFont(size=36))
 corpoDoTexto.grid(row=1, column=0, padx=20, pady = 20)
+
+inserirTexto = CTkEntry(master=app, placeholder_text="Digite uma nova mensagem aqui!",)
+inserirTexto.grid(row=2, column=0, padx=100, pady=20)
+
+inserirBotao = CTkButton(master=app,bg_color="blue",text="Enviar", command=cliqueInserir)
+inserirBotao.grid(row=3, column=1, padx=20, pady=20)
+
 
 
 app.mainloop()
