1fe0bd5f5473ae13dedb4b9cdb64cae00daa788a
Tarik Ponciano <tarikponciano@gmail.com>
2023-05-15 14:17:45 -0300

    Update restApi.py

--- Programador de Sistemas - UC 2/Repositório de Exemplos - Turma 2/Aula 16/restApi.py
+++ Programador de Sistemas - UC 2/Repositório de Exemplos - Turma 2/Aula 16/restApi.py
@@ -5,6 +5,24 @@
 #   - Introdução APIs: https://www.youtube.com/watch?v=umaXYEbd5vA
 #   - API Python - Consulta CEP: https://www.youtube.com/watch?v=SKlF2PmIkrU
 
+#Atividade:
+
+#   - Requisitar os 151 primeiros pokemons
+#   - Guardar num banco de dados postgres
+#   - Atributos: Espécie, Altura, Peso, Tipo e ID
+
 import requests
+
+for i in range(151):
+
+    resultado = requests.get(f"https://pokeapi.co/api/v2/pokemon/{i+1}")
+
+
+    pokeDict = resultado.json()
+
+    print(pokeDict["id"])
+    print(pokeDict["name"])
+    print(pokeDict["height"])
+    print(pokeDict["weight"])
 
 
