35548beac0adae34b4624582a8cfbe28ba63c07f
Tarik Ponciano <tarikponciano@gmail.com>
2023-05-15 14:08:05 -0300

    Create restApi.py

---
+++ Programador de Sistemas - UC 2/Repositório de Exemplos - Turma 2/Aula 16/restApi.py
@@ -0,0 +1,10 @@
+#Recursos
+
+#Vídeos: 
+#   - O que é uma API Rest: https://www.youtube.com/watch?v=lsMQRaeKNDk
+#   - Introdução APIs: https://www.youtube.com/watch?v=umaXYEbd5vA
+#   - API Python - Consulta CEP: https://www.youtube.com/watch?v=SKlF2PmIkrU
+
+import requests
+
+
